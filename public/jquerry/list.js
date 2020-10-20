efe9cea8455632c221d42aa6c4c0d241b86d9dae
unknown <hsc.jr94@gmail.com>
2020-10-20 15:37:55 -0300

    v2

---
+++ public/jquerry/list.js
@@ -0,0 +1,31 @@
+$(".ls").hide();
+
+
+function aparecer() {
+  var chklista = $('input[name="checkbox"]:checked').toArray().map(function(check) {
+        return $(check).val();
+    });
+    var lista = $('input[name="checkbox"]').toArray().map(function(check) {
+          return $(check).val();
+      });
+    //console.log(chklista);
+
+  chklista.forEach((item, i) => {
+        $("#"+item+"").show();
+        if(item === ""){
+          $("#"+item+"").show();
+        }
+
+
+  });
+  lista.forEach((item, i) => {
+    if(chklista.indexOf(item)>=0){
+
+    }else{
+      $("#"+item+"").hide();
+    }
+  });
+
+
+
+}
